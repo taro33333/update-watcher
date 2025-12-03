fc07da80d638baa91190cc70b5543921da4405a1
taro33333 <hahahahawaiwai321@gmail.com>
2025-12-03 15:20:43 +0900

    Add support for additional date formats in date utility; include RFC3339 with nanoseconds and ISO 8601 with milliseconds for enhanced date parsing capabilities.

--- internal/util/date.go
+++ internal/util/date.go
@@ -13,6 +13,8 @@ var SupportedDateFormats = []string{
 	time.RFC3339,
 	time.RFC1123Z,
 	time.RFC1123,
+	"2006-01-02T15:04:05.999999999Z07:00", // RFC3339 with nanoseconds
+	"2006-01-02T15:04:05.999",             // ISO 8601 with milliseconds (NVD format)
 	"2006-01-02T15:04:05Z",
 	"2006-01-02", // Date only format (used by Debian)
 	"Mon, 02 Jan 2006 15:04:05 -0700",
